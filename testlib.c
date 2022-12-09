c6466e3f68937b0637c642431b861c369e01457b
Pierre Mollard <pierre.mollard19@orange.fr>
2022-12-09 16:33:15 +0100

    Added test for library

--- testlib.c
+++ testlib.c
@@ -1,12 +1,19 @@
+#include "src/chilon.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int array_size = 5;
 char* array = "abcdef";
 
 int main(int argc, char* arv[])
 {
+    char * buff = (char*) malloc(64);
+
     for(int i = 0; i < array_size; i++)
     {
-        printf("array[%d]=%c \n", i, array[i]);
+        sprintf(buff, "array[%d]=%c \n", i, array[i]);
+        chilon_print(buff);
     }
+
+    free(buff);
 }
